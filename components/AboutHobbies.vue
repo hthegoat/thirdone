6f59532fd427c04ddfc1e3b7211d7eb05d7f5720
hthegoat <harrisonkug@protonmail.com>
2021-06-30 15:15:24 -0400

    fix

--- components/AboutHobbies.vue
+++ components/AboutHobbies.vue
@@ -21,7 +21,7 @@
       </div>
       <div class="xl:w-1/4 md:w-1/2 p-4">
         <div class="bg-gray-100 p-6 rounded-lg">
-          <img class="h-40 rounded w-full object-cover object-center mb-6" src="~/assets/about/coding.jpg" alt="content">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="~/assets/about/coding.JPG" alt="content">
           <h3 cla
           <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Programming 💻</h2>
           <p class="leading-relaxed text-base">Although not something I am great at, I dabble with coding a few times a year. This website was built using Nuxt and TailwindCSS.</p>
