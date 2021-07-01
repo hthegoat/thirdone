d140014696d737cd95d90eaf3698184756c63601
hthegoat <harrisonkug@protonmail.com>
2021-07-01 13:06:33 -0400

    fix index

--- pages/index.vue
+++ pages/index.vue
@@ -11,26 +11,9 @@
 
 <script>
 
-export default {
-  data() {
-    return {
-      title: 'Harrison Kugler!'
-    }
-  },
-  head() {
-    return {
-      title: this.title,
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content:
-            'The amazing Nuxt application that teaches me all the cool features of Nuxt',
-          image: 'https://i.ibb.co/r2Rt78D/DSC0764-3.jpg',  
-        }
-      ]
-    }
-  }
+
+  
+  
 </script>
 
 <style>
