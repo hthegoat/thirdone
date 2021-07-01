075824297f5175998cafc4c4c15ee8649ef03769
hthegoat <harrisonkug@protonmail.com>
2021-07-01 12:48:51 -0400

    check

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,6 +12,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+
+
       {
         hid: 'Harrison Kugler',
         name: 'Harrison Kugler',
