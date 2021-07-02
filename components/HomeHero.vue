3105633189ea3504b62b141e4f07ccc9c842d68a
hthegoat <harrisonkug@protonmail.com>
2021-07-01 23:25:15 -0400

    ah it didnt work

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="text-gray-600 body-font">
+<section class="text-gray-600 dark:text-white body-font">
   <div class="container mx-auto flex px-5 py-12 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
       <img class="object-cover object-center rounded" alt="hero" src="~/assets/hero2.jpg">
