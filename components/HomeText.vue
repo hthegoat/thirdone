af5f1f1b071d7702286c82281cda8c747cc95b01
hthegoat <harrisonkug@protonmail.com>
2021-07-09 01:15:19 -0400

    fix hometext

--- components/HomeText.vue
+++ components/HomeText.vue
@@ -3,12 +3,12 @@
   <div class=" px-5 py-12 mx-auto">
     <div class="xl:w-1/2 lg:w-full md:w-full mx-auto text-center">
      
-      <p class="leading-relaxed text-xl font-bold text-gray-900 whitespace-pre-line m-2">Social Media and Marketing provoke a wide array of emotions for entrepreneurs and business owners. In the past 6 years as a marketer I have seen tons of different marketing scenarios that have left me shaking my head. 
+      <p class="leading-relaxed text-xl font-bold text-gray-900 dark:text-gray-300 whitespace-pre-line m-2">Social Media and Marketing provoke a wide array of emotions for entrepreneurs and business owners. In the past 6 years as a marketer I have seen tons of different marketing scenarios that have left me shaking my head. 
 </p>
- <p class="leading-relaxed text-2xl font-black text-gray-900 whitespace-pre-line">  
+ <p class="leading-relaxed text-2xl font-black text-gray-900 dark:text-gray-300 whitespace-pre-line">  
   My mission is to help companies create business value through emotional currency. 
 </p>
- <p class="leading-relaxed text-xl font-bold text-gray-900 whitespace-pre-line">     
+ <p class="leading-relaxed text-xl font-bold text-gray-900 dark:text-gray-300 whitespace-pre-line">     
  In the past 3 years I have generated roughly 2.5 million in revenue for clients ranging from axe throwing venues, to NFT startups. My experience is mostly with Facebook/Instagram Ads but I have spent five figures on most ad platforms including TikTok, Snapchat, Google/Youtube, and LinkedIn. 
 
       I have worked on projects as a Paid Social Specialist as well as a Marketing Director Consultant and can reliably get the job done. I can work with a team or solo and am a great communicator. 
